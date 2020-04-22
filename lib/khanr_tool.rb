8b2a1749ee74e0223fbef9c4f63b7890aa8799d5
RizaHKhan <khanriza@gmail.com>
2020-04-21 22:53:53 -0400

    Initial commit

---
+++ lib/khanr_tool.rb
@@ -0,0 +1,7 @@
+require "khanr_tool/version"
+require "khanr_tool/renderer"
+
+module KhanrTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/khanr_tool/version.rb
@@ -0,0 +1,3 @@
+module KhanrTool
+  VERSION = "0.1.0"
+end
